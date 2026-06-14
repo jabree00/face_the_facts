40a2b3095870f4998835314b70bf1525e641921b
jabree00 <jabreeellis@gmail.com>
2026-06-14 18:59:32 -0400

    Rough Draft

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# face_the_facts
+<h1>READ ME! (please)</h1>
+<p>Face The Facts is a script that identifies package vulnerabilites within a given project.</p>
+
+<h2>Resources Consulted</h2>
+<ul>
+    <li><a href="https://pypi.org/project/requests/">Using Python requests package</a></li>
+    <li><a href="https://nvd.nist.gov/developers/vulnerabilities">Using NIST Vulnerability API</a></li>
+    <li><a href="https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=pip%20python">Sample NIST API Endpoint</a></li>
+    <li><a href="https://www.w3schools.com/python/python_dictionaries_loop.asp">Looping through Python Dictionaries</a></li>
+    <li><a href="https://www.w3schools.com/tags/tag_a.asp">Creating a tag elements</a></li>
+    <li><a href="https://www.w3schools.com/python/ref_list_append.asp">Appending to Python list</a></li>
+    <li><a href="https://www.w3schools.com/python/python_json.asp">Importing JSON package</a></li>
+    <li><a href="https://www.geeksforgeeks.org/python/python-list-files-in-a-directory/">Listing files in a directory</a></li>
+    <li><a href="https://www.w3schools.com/python/ref_list_pop.asp">Pop from list python</a></li>
+    <li><a href="https://www.geeksforgeeks.org/python/python-ways-to-concatenate-two-lists/">Merge Python Lists</a></li>
+    <li><a href="https://www.w3schools.com/python/python_match.asp">Python Matches</a><li>
+
+
+</ul>
+
+<h2>AI Disclosure</h2>
+AI (Gemini) was used to conduct an initial search of tools that perform similar tasks. 
+
+Here was the prompt was...
+Are there any apps that statically scan a python project for imports and tell the user what known vulnerabilities exist in the imported packages?
+
+My follow-up prompts focused on verifying that these tools compare against a CVE database and determining whether these tools cost money. 
+
--- face_the_facts.py
+++ face_the_facts.py
@@ -1,23 +1,75 @@
-def recursively_scan_project(project):
-    pass 
+import requests
+import json
+import os 
 
+'''
+Given a directory, find CVES of all identified files
+'''
+def recursively_scan_project(dir):
+    imports_listing = []
+    dir_listing = os.listdir(dir)
+
+    index = len(dir_listing) - 1
+    while (dir_listing != []):
+        #handle files 
+        if(os.path.isfile(dir_listing)):
+            imports_listing + scan_file(dir_listing)
+        
+        #handle directories 
+        elif(os.path.isdir(dir_listing)):
+            imports_listing + recursively_scan_project(dir_listing)
+
+        dir_listing.pop(index)
+        index = index - 1
+
+    return imports_listing 
+
+'''
+Given a file, find CVES of all identified imports 
+'''
 def scan_file(file):
     f = open(file,"r")
     file_contents = f.read(); 
     regex = r""
     matches_list = file_contents.match(regex)
-    for match in matches_list:
-        grab_cves(match)
+    return matches_list
 
+'''
+Give an package name conduct a search
+'''
+def query_nist_database(imported_package):
+    BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+    QUERY_PARAMETER = "keywordSearch=" + imported_package + " " + "python"
+    QUERY_PARAMETER = QUERY_PARAMETER.replace(" ","%20")
+    FULL_URL = BASE_URL + "?" + QUERY_PARAMETER
+    response = requests.get(FULL_URL).json
+    cve_data = extract_cves(response)
+    return cve_data
 
-def grab_cves():
-    pass
-
+def extract_cves(json_data):
+    cve_data = []
+    relevant_json = json_data.get("vulnerabilities")
+    for current in relevant_json:
+        cve = current.get("cve")
+        cve_id = cve.get("id")
+        cve_description = cve.get("description")
+        cve_dict = {
+            "id": cve_id, 
+            "description": cve_description
+        }
+        cve_data.append(cve_dict)
+    return cve_data
 
 def main(): 
     intro = '''FACE THE FACTS...
-    Your project has vulnerabilities. 
+    Your project probably has vulnerabilities.
+    This tool can help by comparing imports in your project against the
+    NIST database. 
     '''
     print(intro)
-    recursively_scan_rpject
+
+    all_imports = recursively_scan_project()
+    for imp in all_imports:
+        query_nist_database(imp)
+
 
